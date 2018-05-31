278256c973984de4c68062c56b409443b1aaeac4
Mark Harrison <mark@mivok.net>
2018-05-30 22:40:41 -0400

    Implement the API request directly

--- main.go
+++ main.go
@@ -3,20 +3,76 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/BurntSushi/toml"
-	"github.com/jsgoecke/nest"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Config struct {
-	ClientID     string `toml:"client_id"`
-	ClientSecret string `toml:"client_secret"`
-	Token        string
+	Token           string
+	RefreshInterval int `toml:"refresh_interval"`
+}
+
+type Devices struct {
+	Thermostats map[string]Device
+}
+
+type Device struct {
+	Humidity                  float64
+	Locale                    string
+	TemperatureScale          string `json:"temperature_scale"`
+	IsUsingEmergencyHeat      bool   `json:"is_using_emergency_heat"`
+	HasFan                    bool   `json:"has_fan"`
+	SoftwareVersion           string `json:"software_version"`
+	HasLeaf                   bool   `json:"has_leaf"`
+	WhereId                   string `json:"where_id"`
+	DeviceId                  string `json:"device_id"`
+	Name                      string
+	CanHeat                   bool    `json:"can_heat"`
+	CanCool                   bool    `json:"can_cool"`
+	TargetTemperatureC        float64 `json:"target_temperature_c"`
+	TargetTemperatureF        float64 `json:"target_temperature_f"`
+	TargetTemperatureHighC    float64 `json:"target_temperature_high_c"`
+	TargetTemperatureHighF    float64 `json:"target_temperature_high_f"`
+	TargetTemperatureLowC     float64 `json:"target_temperature_low_c"`
+	TargetTemperatureLowF     float64 `json:"target_temperature_low_f"`
+	AmbientTemperatureC       float64 `json:"ambient_temperature_c"`
+	AmbientTemperatureF       float64 `json:"ambient_temperature_f"`
+	AwayTemperatureHighC      float64 `json:"away_temperature_high_c"`
+	AwayTemperatureHighF      float64 `json:"away_temperature_high_f"`
+	AwayTemperatureLowC       float64 `json:"away_temperature_low_c"`
+	AwayTemperatureLowF       float64 `json:"away_temperature_low_f"`
+	EcoTemperatureHighC       float64 `json:"eco_temperature_high_c"`
+	EcoTemperatureHighF       float64 `json:"eco_temperature_high_f"`
+	EcoTemperatureLowC        float64 `json:"eco_temperature_low_c"`
+	EcoTemperatureLowF        float64 `json:"eco_temperature_low_f"`
+	IsLocked                  bool    `json:"is_locked"`
+	LockedTempMinC            float64 `json:_min_c"`
+	LockedTempMinF            float64 `json:"locked_temp_min_f"`
+	LockedTempMaxC            float64 `json:"locked_temp_max_c"`
+	LockedTempMaxF            float64 `json:"locked_temp_max_f"`
+	SunlightCorrectionActive  bool    `json:"sunlight_correction_active"`
+	SunlightCorrectionEnabled bool    `json:"sunlight_correction_enabled"`
+	StructureId               string  `json:"structure_id"`
+	FanTimerActive            bool    `json:"fan_timer_active"`
+	FanTimerTimeout           string  `json:"fan_timer_timeout"`
+	FanTimerDuration          float64 `json:"fan_timer_duration"`
+	PreviousHvacMode          string  `json:"previous_hvac_mode"`
+	HvacMode                  string  `json:"hvac_mode"`
+	TimeToTarget              string  `json:"time_to_target"`
+	TimeToTargetTraining      string  `json:"time_to_target_training"`
+	WhereName                 string  `json:"where_name"`
+	Label                     string
+	NameLong                  string `json:"name_long"`
+	IsOnline                  bool   `json:"is_online"`
+	LastConnection            string `json:"last_connection"`
+	HvacState                 string `json:"hvac_state"`
 }
 
 // Prometheus stats
@@ -74,6 +130,11 @@ var (
 		"Path to the configuration file.")
 )
 
+// Other
+var (
+	CachedRedirectURL string // because nest wants you to reuse the redirect URL
+)
+
 func init() {
 	flag.Parse()
 	prometheus.MustRegister(stateStat)
@@ -84,26 +145,57 @@ func init() {
 	prometheus.MustRegister(hvacStateStat)
 }
 
+func getDevices(token string) (*Devices, error) {
+	// See https://developers.nest.com/documentation/cloud/how-to-handle-redirects#store_the_redirected_location
+	// for why there is a cached redirect URL
+	url := CachedRedirectURL
+	if url == "" {
+		url = "https://developer-api.nest.com/devices.json/?auth=" + token
+	}
+	resp, err := http.Get(url)
+	respURL := resp.Request.URL.String()
+	if respURL != url {
+		// We were redirected, so cache the new URL
+		CachedRedirectURL = respURL
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP code %d: %s", resp.StatusCode, body)
+	}
+	devices := &Devices{}
+	err = json.Unmarshal(body, devices)
+	if err != nil {
+		return nil, err
+	}
+	return devices, nil
+}
+
 func main() {
 	var config Config
 	if _, err := toml.DecodeFile(*configFile, &config); err != nil {
 		log.Fatal(err)
 	}
-	client := nest.New(config.ClientID, "STATE", config.ClientSecret, "")
-	client.Token = config.Token
-	ticker := time.NewTicker(60 * time.Second)
+	if config.RefreshInterval == 0 {
+		// Default to 1 minute refreshes
+		config.RefreshInterval = 60
+	}
+	ticker := time.NewTicker(time.Duration(config.RefreshInterval) *
+		time.Second)
 	go func() {
 		for {
-			devices, err := client.Devices()
+			devices, err := getDevices(config.Token)
 			if err != nil {
-				errContent, jsonErr := json.Marshal(err)
-				if jsonErr != nil {
-					log.Fatal("Unknown API Error")
-				}
-				log.Println("API Error:", string(errContent))
+				log.Println(err)
 			} else {
 				for _, t := range devices.Thermostats {
-					// States
+					// States - 1 for on, 0 for off
 					var isOnline float64
 					var canCool float64
 					var canHeat float64
@@ -170,21 +262,21 @@ func main() {
 					if t.HvacMode == "heat" || t.HvacMode == "cool" {
 						tts.With(prometheus.Labels{
 							"type": "target_temperature",
-						}).Set(float64(t.TargetTemperatureF))
+						}).Set(t.TargetTemperatureF)
 					} else if t.HvacMode == "heat-cool" {
 						tts.With(prometheus.Labels{
 							"type": "target_temperature_high",
-						}).Set(float64(t.TargetTemperatureHighF))
+						}).Set(t.TargetTemperatureHighF)
 						tts.With(prometheus.Labels{
 							"type": "target_temperature_low",
-						}).Set(float64(t.TargetTemperatureLowF))
+						}).Set(t.TargetTemperatureLowF)
 					} else if t.HvacMode == "eco" {
 						tts.With(prometheus.Labels{
 							"type": "away_temperature_high",
-						}).Set(float64(t.AwayTemperatureHighF))
+						}).Set(t.AwayTemperatureHighF)
 						tts.With(prometheus.Labels{
 							"type": "away_temperature_low",
-						}).Set(float64(t.AwayTemperatureLowF))
+						}).Set(t.AwayTemperatureLowF)
 					}
 
 					// The hvac state and mode stats are implemented by having
@@ -202,7 +294,7 @@ func main() {
 
 					humidityStat.With(
 						prometheus.Labels{"thermostat": t.Name}).Set(
-						float64(t.Humidity))
+						t.Humidity)
 				}
 			}
 			// Wait until the next tick
